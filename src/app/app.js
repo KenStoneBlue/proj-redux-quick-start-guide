f9a51df60455a52a2dc9e7e85d557e8b8ca54aa5
Ken Stone <ken@stoneblue.com>
2020-04-15 14:45:47 -0500

    Toggle button and render sotre state

--- src/app/app.js
+++ src/app/app.js
@@ -7,6 +7,30 @@ console.log("Initially tubelight is: ", store.getState());
 store.dispatch({ type: "TURN_ON" });
 console.log("Now tubelight is: ", store.getState());
 
+const button = document.createElement("button");
+button.setAttribute("id", "lightButton");
+var text = document.createTextNode("Toggle Light");
+button.appendChild(text);
+document.body.appendChild(button);
+
+
+document.getElementById("lightButton").addEventListener("click", () => {
+ if (store.getState() === "ON") {
+   store.dispatch({ type: "TURN_OFF" });
+ } else {
+   store.dispatch({ type: "TURN_ON" });
+ }
+});
+
+
 
 
 console.log("welc f s");
+
+const render = () => {
+ document.body.innerText = store.getState();
+ document.body.appendChild(button);
+};
+store.subscribe(render);
+render();
+
